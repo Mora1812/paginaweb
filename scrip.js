b1d9d20fcefeaa1d945ebc52f6313f3c5032a2ea
Mora1812 <maria1mora1812@gmail.com>
2024-12-02 01:07:05 -0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,16 @@
+var swiper= new swiper(".mySwiper-1",{
+    slidesPerview:1,
+    spaceBetween:30,
+    loo: true,
+    pagination:{
+        el:".swiper-pagination",
+        clickable:true,
+
+    },
+    navigation:{
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev"
+    }
+}
+
+)
